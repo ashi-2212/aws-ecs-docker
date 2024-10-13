7a6d59f0d7f5cb53125a46ac30a18d5b4daa31d2
Ramtilakapp <ramtilak.yadav@appinventiv.com>
2024-10-13 17:14:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to Ram Tilak Yadav Application which is deployed on ECS through FARGET using AWS Codepipeline!!!!!!");
+  res.send("Welcome to Ram Tilak Yadav Node Application which is deployed on ECS through EC2 using AWS Codepipeline!!!!!!");
 });
 
 app.get("/info", (req, res) => {
